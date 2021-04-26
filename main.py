33ee93eede74e3751b5187d00e6eb4a35d21878e
yetione <jub45@yandex.ru>
2021-04-26 21:48:35 +0300

    add tables

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import re
+from pandas import *
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tableONE = ExcelFile(r'./full.xlsx')
+tableTWO = ExcelFile(r'./test.xlsx')
+FirstTable = tableONE.parse(tableONE.sheet_names[0])
+SecondTable = tableTWO.parse(tableTWO.sheet_names[0])
+regex = '[-]'
+results = [v for k, v in FirstTable.items() if re.match (regex, k)]
+print (results)
+#print (FirstTable.to_dict)
+#print (SecondTable.to_dict)
+k=input("waiting")
